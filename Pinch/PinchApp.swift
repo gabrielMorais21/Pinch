ffe60196fe6b4d2f5c06e9720f3162d6a0aaa8f7
gabriel.dias <gabriel.dias@granitopagamentos.com.br>
2023-07-12 22:58:27 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Gabriel Morais Dias on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
